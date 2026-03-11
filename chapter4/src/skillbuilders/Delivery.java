75cd1de997ebd75c23e697a05e1fb8b73dcf4943
410015008 <410015008@L815WD220012.edu.cbe.ab.ca>
2026-03-11 13:07:58 -0600

    1:07PM

--- chapter4/src/skillbuilders/Delivery.java
+++ chapter4/src/skillbuilders/Delivery.java
@@ -1,10 +1,58 @@
 package skillbuilders;
 
+import java.util.Scanner;
+
 public class Delivery {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter the length of the package: ");
+		int length = scanner.nextInt();
+		
+		System.out.println("Enter the width of the package: ");
+		int width = scanner.nextInt();
+		
+		System.out.println("Enter the height of the package: ");
+		int height = scanner.nextInt();
+		
+		int value;
+		value = length + height + width;
+		
+		int failed;
+		
+	if (length <= 10) {
+			
+			int lengthValid;
+			
+			System.out.println("passed");
+		}
+	else System.out.println("failed");
+	
 
+	if (width <= 10) {
+		
+		int widthValid;
+		
+		System.out.println("passed");
 	}
+else System.out.println("failed");
 
-}
+
+if (height <= 10) {
+		
+		int heigthValid;
+		
+		System.out.println("passed");
+	}
+else System.out.println("failed");
+
+if (value <= 30) {
+		
+		int valueValid;
+		
+		System.out.println("THIS PACKAGE IS ALLOWED FOR DELIVERY!");
+	}
+else System.out.println("THIS PACKAGE IS NOT ALLOWED FOR DELIVERY!");
+}}
