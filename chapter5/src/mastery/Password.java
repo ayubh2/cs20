5071681c9e6ace00936cf532909e4300edf2cb19
410015008 <410015008@L815WD220012.edu.cbe.ab.ca>
2026-03-17 14:25:40 -0600

    2:25PM

--- chapter5/src/mastery/Password.java
+++ chapter5/src/mastery/Password.java
@@ -1,10 +1,19 @@
 package mastery;
 
+import java.util.Scanner;
+
 public class Password {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the password: ");
+		int password = 6767;
+		
+		if (password == 6767) {
+			System.out.println("Welcome");
+			}
+		else System.out.println("The password you typed is incorrect");
 	}
 
 }
