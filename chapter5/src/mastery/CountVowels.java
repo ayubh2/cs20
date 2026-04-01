f61bf86a58973e311688710d7f6868459ead5331
410015008 <410015008@L815WD220012.edu.cbe.ab.ca>
2026-04-01 13:30:58 -0600

    1:30PM

--- chapter5/src/mastery/CountVowels.java
+++ chapter5/src/mastery/CountVowels.java
@@ -7,10 +7,18 @@ public class CountVowels {
 	public static void main(String[] args) 
 	{
 		Scanner scanner = new Scanner(System.in);
+		
+		String text;
+		int amount = 0;
+		
 		System.out.println("Enter text: ");
-		String password = scanner.next();
+	    text = scanner.next();
 		
-		
+	    for(int i = 1; i <= amount; i += 2)
+		{
+			amount = amount + i;
+		}
+		System.out.println("The number of vowels in "+ text + " is " + amount);
 
 	}
 
