17c71c35cbaf2ab1a74667d28d9a6481dd118ddf
410015008 <410015008@L815WD220012.edu.cbe.ab.ca>
2026-04-01 13:06:38 -0600

    1:06PM

--- chapter5/src/mastery/CountVowels.java
+++ chapter5/src/mastery/CountVowels.java
@@ -1,10 +1,16 @@
 package mastery;
 
+import java.util.Scanner;
+
 public class CountVowels {
 
 	public static void main(String[] args) 
 	{
-	
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter text: ");
+		String password = scanner.next();
+		
+		
 
 	}
 
--- chapter5/src/skillbuilders/NumbersSum.java
+++ chapter5/src/skillbuilders/NumbersSum.java
@@ -6,10 +6,7 @@ public class NumbersSum {
 
 	public static void main(String[] args) 
 	{
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter a number: ");
-		int number = scanner.nextInt();
-        
+		
 	}
 
 }
