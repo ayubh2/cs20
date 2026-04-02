fd0116dc9af9d492cca2b9141310fc9593f09c2b
410015008 <410015008@L815WD220012.edu.cbe.ab.ca>
2026-04-02 15:18:56 -0600

    3:18PM

---
+++ chapter5/src/mastery/Investment.java
@@ -0,0 +1,16 @@
+package mastery;
+
+import java.util.Scanner;
+
+public class Investment {
+
+	public static void main(String[] args) 
+	{   
+	   int years = 9;
+	   int invesment = 2500;
+	   
+	   System.out.println("It will take " + years + " years to reach $5000 if invested $" + invesment + " and compounded annually at 7.5%");
+
+	}
+
+}
