9168db975f2ebf07d96544c553a3f6ae8713b08b
410015008 <410015008@L815WD220012.edu.cbe.ab.ca>
2026-04-06 13:31:15 -0600

    1:31PM

--- chapter6/src/skillbuilders/TimeConverter.java
+++ chapter6/src/skillbuilders/TimeConverter.java
@@ -1,11 +1,15 @@
 package skillbuilders;
 
+import java.util.Scanner;
+
 public class TimeConverter {
 
 	public static void main(String[] args) 
 	{
-	  
+	  double days, hours, minutes;
+	  Scanner input = new Scanner(System.in);
           
+	  System.out.print("Enter the days: ");
 	}
 
 }
