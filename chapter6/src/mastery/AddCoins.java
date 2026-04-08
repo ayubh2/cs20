03bcbca70403c67b81f550529e2fee4a698d72d7
410015008 <410015008@L815WD220012.edu.cbe.ab.ca>
2026-04-08 12:50:26 -0600

    12:50PM

--- chapter6/src/mastery/AddCoins.java
+++ chapter6/src/mastery/AddCoins.java
@@ -1,3 +1,17 @@
+/*
+
+Program: AddCoins.java          Last Date of this Revision: April 8, 2026
+
+Purpose: An application that asks the users to input the number of pennies, nickels, dimes and quarters 
+         and then displays the total dollar amount.
+
+Author: Ayub Handule 
+School: CHHS
+Course: Computer Science 20
+ 
+
+*/
+
 package mastery;
 
 public class AddCoins {
--- chapter6/src/mastery/PerfectIntegers.java
+++ chapter6/src/mastery/PerfectIntegers.java
@@ -1,10 +1,23 @@
+/*
+
+Program: PerfectIntegers.java          Last Date of this Revision: April 8, 2026
+
+Purpose: An application that displays all perfect integers up to 100.
+
+Author: Ayub Handule 
+School: CHHS
+Course: Computer Science 20
+ 
+
+*/
+
 package mastery;
 
 public class PerfectIntegers {
 
 	public static void main(String[] args)
 	{
-	
+		
 
 	}
 
