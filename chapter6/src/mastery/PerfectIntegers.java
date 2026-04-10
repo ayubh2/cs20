741763ca3aa4ff2f00ce6989bbc6d7001904806b
410015008 <410015008@L815WD220012.edu.cbe.ab.ca>
2026-04-10 11:02:10 -0600

    11:02AM

--- chapter6/src/mastery/PerfectIntegers.java
+++ chapter6/src/mastery/PerfectIntegers.java
@@ -1,6 +1,6 @@
 /*
 
-Program: PerfectIntegers.java          Last Date of this Revision: April 8, 2026
+Program: PerfectIntegers.java          Last Date of this Revision: April 10, 2026
 
 Purpose: An application that displays all perfect integers up to 100.
 
@@ -14,17 +14,53 @@ Course: Computer Science 20
 package mastery;
 
 public class PerfectIntegers {
-
-	public static void main(String[] args)
+	
+	public static boolean isPerfect(int num)
 	{
+		int sum = 0;
+		int divisor;
 		
+		for(divisor = 1; divisor < num; divisor++)
+		{
+			if(isFactor(num, divisor))
+			{
+				sum = sum + divisor;
+			}
+		}
+		if(sum == num)
+			return(true);
+		else
+			return(false);	
+		
+	}
+	// does the calculations
+	public static boolean isFactor(int dividend, int divisor)
+	{
+		if(dividend % divisor == 0)
+			return(true);
+		else
+			return(false);
+	}
+	
 
+	// makes sure the number for the perfect integers counted are only up to 100 and a minumum of 1.
+	public static void main(String[] args) 
+	{
+		for (int num = 1; num <=100; num++)
+		{
+			if(isPerfect(num))
+			{
+				System.out.println(num);
+			}
+		}
+		
 	}
 
 }
 
 /* Screen Dump
 
-Paste the output of your code here
+6
+28
  
  */
