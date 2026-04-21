821fce0609995b6fa9ddf3bda8f4dcc56101328f
410015008 <410015008@L815WD220012.edu.cbe.ab.ca>
2026-04-21 15:20:03 -0600

    3:20PM

--- chapter7/src/mastery/MySavings.java
+++ chapter7/src/mastery/MySavings.java
@@ -4,7 +4,14 @@ public class MySavings {
 
 	public static void main(String[] args) 
 	{
-		
+		System.out.println("1. Show total in bank.");
+		System.out.println("2. Add a penny");
+		System.out.println("3. Add a nickel");
+		System.out.println("4. Add a dime");
+		System.out.println("5. Add a quarter");
+		System.out.println("6. Take money out of bank.");
+
+		System.out.println("Enter Your Choice: ");
 
 	}
 
--- chapter9/src/skillbuilders/FriendsList.java
+++ chapter9/src/skillbuilders/FriendsList.java
@@ -1,16 +1,29 @@
 package skillbuilders;
 
-public class FriendsList 
-{
+public class FriendsList {
 
 	public static void main(String[] args) 
 	{
-		 
-		//Declare and then allocate space
-		String[] friends = new String[5];
+		//String[] friends = new String[5];
 		
-		//Initialize with values
-		String[] friends = {"Kermit" , "Vlad" , "Ahmed" , "Lisa" , "Gurshaan"};
+		int[] squares = new int [11];
+		
+		
+		//To fill the array
+		for(int i = 0; i < squares.length; i++)
+		{
+			squares[i] = i * i;
+		}
+		
+		//To dusplay values from the array
+		for(int val: squares)
+		{
+			System.out.println(val);
+		}
+		
+		
+		
+		
 		
     }
 
