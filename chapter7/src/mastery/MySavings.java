cdcb9867441c367922c8853a051d0771acd65b23
410015008 <410015008@L815WD220012.edu.cbe.ab.ca>
2026-04-27 13:30:02 -0600

    1:30PM

--- chapter7/src/mastery/MySavings.java
+++ chapter7/src/mastery/MySavings.java
@@ -1,17 +1,40 @@
 package mastery;
 
+import java.util.Scanner;
+
 public class MySavings {
 
 	public static void main(String[] args) 
 	{
+		Scanner scanner = new Scanner(System.in);
+
+		double amount = 0;
+		
 		System.out.println("1. Show total in bank.");
 		System.out.println("2. Add a penny");
 		System.out.println("3. Add a nickel");
 		System.out.println("4. Add a dime");
 		System.out.println("5. Add a quarter");
-		System.out.println("6. Take money out of bank.");
-
+		System.out.println("6. Take money out of bank");
+		System.out.println("Enter 0 to quit");
 		System.out.println("Enter Your Choice: ");
+		int input = scanner.nextInt();
+		
+
+		if (input == 1) 
+		{
+
+        System.out.println("Your total amount in bank is: $" + amount); 
+        
+		}
+		
+		if (input == 2) 
+		{
+
+        amount = amount + 0.01;
+		System.out.println("Succesfully Added! Your new total amount in bank is: $" + amount); 
+        
+		}
 
 	}
 
