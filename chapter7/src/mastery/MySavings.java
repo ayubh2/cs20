6175795a1b4c57e815c916ea029f38d2d7245e42
410015008 <410015008@L815WD220012.edu.cbe.ab.ca>
2026-04-29 13:08:23 -0600

    1:08PM

--- chapter7/src/mastery/MySavings.java
+++ chapter7/src/mastery/MySavings.java
@@ -35,7 +35,43 @@ public class MySavings {
 		System.out.println("Succesfully Added! Your new total amount in bank is: $" + amount); 
         
 		}
+		
+		if (input == 3) 
+		{
 
+        amount = amount + 0.05;
+		System.out.println("Succesfully Added! Your new total amount in bank is: $" + amount); 
+        
+		}
+
+		if (input == 4) 
+		{
+
+        amount = amount + 0.10;
+		System.out.println("Succesfully Added! Your new total amount in bank is: $" + amount); 
+        
+		}
+	
+		if (input == 5) 
+		{
+
+        amount = amount + 0.25;
+		System.out.println("Succesfully Added! Your new total amount in bank is: $" + amount); 
+        
+		}
+		
+		if (input == 6) 
+		{
+          
+	    System.out.println("How Much would you like to take out?");
+	    int take = scanner.nextInt();
+	    
+	    if (take >= amount)
+        amount = amount - take;
+		System.out.println("Succesfully Taken! You took: $" + take); 
+		
+        
+		}
 	}
 
 }
