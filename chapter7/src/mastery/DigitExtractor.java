69bad65b57b50420315060cdeb7b8792dc51fdf4
410015008 <410015008@L815WD220012.edu.cbe.ab.ca>
2026-05-04 13:41:17 -0600

    1:41PM

--- chapter7/src/mastery/DigitExtractor.java
+++ chapter7/src/mastery/DigitExtractor.java
@@ -1,3 +1,17 @@
+/*
+
+Program: DigitExtractor.java          Last Date of this Revision: May 4, 2026
+
+Purpose: An application that asks the user
+
+Author: Ayub Handule 
+School: CHHS
+Course: Computer Science 20
+ 
+
+*/
+
+
 package mastery;
 
 public class DigitExtractor {
