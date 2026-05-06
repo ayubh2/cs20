9e23fead432212a74a6114dc4129436415be4098
410015008 <410015008@L815WD220012.edu.cbe.ab.ca>
2026-05-06 13:03:04 -0600

    1:03PM

--- chapter7/src/mastery/DigitExtractor.java
+++ chapter7/src/mastery/DigitExtractor.java
@@ -21,12 +21,12 @@ public class DigitExtractor {
 	public static void main(String[] args) 
 	{
 		
-		
+		// asks the user for an integer and makes sure its an integer
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter an integer: ");
 		int integer = scanner.nextInt();
 
-		
+		// asks the user to choose one of the options below and then scanning to see what they chose
 		System.out.println("show (W)hole number.");
 		System.out.println("show (O)nes place number.");
 		System.out.println("show (T)ens place number.");
@@ -35,6 +35,7 @@ public class DigitExtractor {
 		System.out.println("Enter your choice: ");
 		String choice = scanner.next();
 		
+		// depending on their choice they will get their answer it makes sure the value is correctly inputed and calculates the result
 		if (choice.equals("W"))
 		{
 	        System.out.println("Answer: " + integer); 
