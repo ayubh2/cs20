a6d7a7862c61151cb41d45bc0cab694ee795e248
410015008 <410015008@L815WD220012.edu.cbe.ab.ca>
2026-05-12 15:15:49 -0600

    3:15PM

--- chapter9/src/mastery/EvensAndOdds.java
+++ chapter9/src/mastery/EvensAndOdds.java
@@ -1,3 +1,16 @@
+/*
+
+Program: EvensAndOdds.java          Last Date of this Revision: May 12, 2026
+
+Purpose: Generates 25 random integers between 0 and 99 and then displays all the evens on one line and all the odds on the next line.
+
+Author: Ayub Handule 
+School: CHHS
+Course: Computer Science 20
+ 
+
+*/
+
 package mastery;
 
 public class EvensAndOdds {
--- chapter9/src/mastery/RandomStats.java
+++ chapter9/src/mastery/RandomStats.java
@@ -1,3 +1,16 @@
+/*
+
+Program: RandomStats.java          Last Date of this Revision: May 12, 2026
+
+Purpose: Generates 500 random numbers between 0 and 9 and then displays the number of occurrences of each number.
+
+Author: Ayub Handule 
+School: CHHS
+Course: Computer Science 20
+ 
+
+*/
+
 package mastery;
 
 public class RandomStats {
