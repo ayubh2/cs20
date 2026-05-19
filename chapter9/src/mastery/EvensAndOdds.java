e341731248696a39901c06950c384e11c10ec300
410015008 <410015008@L815WD220012.edu.cbe.ab.ca>
2026-05-19 15:27:39 -0600

    3:27PM

--- chapter9/src/mastery/EvensAndOdds.java
+++ chapter9/src/mastery/EvensAndOdds.java
@@ -1,6 +1,6 @@
 /*
 
-Program: EvensAndOdds.java          Last Date of this Revision: May 12, 2026
+Program: EvensAndOdds.java          Last Date of this Revision: May 19, 2026
 
 Purpose: Generates 25 random integers between 0 and 99 and then displays all the evens on one line and all the odds on the next line.
 
