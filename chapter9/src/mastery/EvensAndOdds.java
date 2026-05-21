f97dc16a1647dfe994f88b9ea3d66b74118ae46d
410015008 <410015008@L815WD220012.edu.cbe.ab.ca>
2026-05-21 15:00:13 -0600

    3:00PM

--- chapter9/src/mastery/EvensAndOdds.java
+++ chapter9/src/mastery/EvensAndOdds.java
@@ -13,16 +13,39 @@ Course: Computer Science 20
 
 package mastery;
 
-import java.util.Scanner;
+import java.util.Random; 
+
 
 public class EvensAndOdds {
 
-	public static void main(String[] args) 
-	{
-	      int[] integers = {0,1,2,3};
-	      System.out.println(integers[0]);
-		
+	    public static void main(String[] args) {
+	       
+	       // Generates 25 random numbers between 0-99
+		    Random random = new Random();
+	        int[] numbers = new int[25];
 
+	       // makes sure the numbers are between 0 and 99
+	        for (int i = 0; i < numbers.length; i++) {
+	            numbers[i] = random.nextInt(100);
+	        }
+
+	       // prints the even numbers after finding them
+	        System.out.print("Even Numbers: ");
+	        for (int num : numbers) {
+	            if (num % 2 == 0) {
+	                System.out.print(num + " ");
+	            }
+	        }
+	        System.out.println(); 
+
+	        // prints the odd numbers after finding them
+	        System.out.print("Odd Numbers: ");
+	        for (int num : numbers) {
+	            if (num % 2 != 0) {
+	                System.out.print(num + " ");
+	            }
+	        }
+	        System.out.println();
 		
 	}
 
